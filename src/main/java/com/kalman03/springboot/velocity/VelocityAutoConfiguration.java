4c799d3ae97b3092513a8613a465e3529095e3e6
pugwoo <pugwoo@gmail.com>
2024-02-05 22:25:44 +0800

    Set velocity deprecation logger level to error

--- src/main/java/com/kalman03/springboot/velocity/VelocityAutoConfiguration.java
+++ src/main/java/com/kalman03/springboot/velocity/VelocityAutoConfiguration.java
@@ -1,10 +1,12 @@
 package com.kalman03.springboot.velocity;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import ch.qos.logback.classic.Level;
+import com.kalman03.springboot.velocity.view.VelocityViewResolver;
 import org.apache.velocity.app.Velocity;
+import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.spring.VelocityEngineFactoryBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -12,7 +14,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
 
-import com.kalman03.springboot.velocity.view.VelocityViewResolver;
+import java.util.HashMap;
+import java.util.Map;
 
 @AutoConfiguration
 public class VelocityAutoConfiguration {
@@ -25,6 +28,14 @@ public class VelocityAutoConfiguration {
 
 	@Bean
 	VelocityEngineFactoryBean velocityEngineFactoryBean(VelocityProperty velocityProperty) {
+
+		// Set velocity deprecation logger level to error
+		Logger logger = LoggerFactory.getLogger(RuntimeConstants.DEFAULT_RUNTIME_LOG_NAME + ".deprecation");
+		if (logger instanceof ch.qos.logback.classic.Logger) {
+			ch.qos.logback.classic.Logger logbackLogger = (ch.qos.logback.classic.Logger) logger;
+			logbackLogger.setLevel(Level.ERROR);
+		}
+
 		VelocityEngineFactoryBean bean = new VelocityEngineFactoryBean();
 		bean.setResourceLoaderPath(velocityProperty.getResourceLoaderPath());
 		Map<String, Object> velocityPropertiesMap = new HashMap<>();
